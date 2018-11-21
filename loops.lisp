d5306e4aa1a2e58f56e2e89667e5170ba90b979a
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-11-20 23:57:54 -0500

    Add comments to loops

--- loops.lisp
+++ loops.lisp
@@ -1,10 +1,15 @@
 (write-line "Input 10 numbers to put into an array, pressing enter after each one")
 
+; Define array of length 10
 (setf num-array (make-array'(10)))
+
+; Get 10 integers from the user
 (dotimes (itr 10)
+    ; Put each number into the array
     (setf (aref num-array itr) (read))
 )
 
+; Output array
 (write "Your array: ")
 (write num-array)
 (terpri)
