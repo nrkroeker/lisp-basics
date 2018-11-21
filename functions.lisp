6dce50be4291acfbe75112cb8521f55ff6ad4d1d
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-11-20 23:55:04 -0500

    Add comments to functions

--- functions.lisp
+++ functions.lisp
@@ -1,13 +1,18 @@
+; Define array of length 10
 (setf num-array (make-array`(10)))
 (write-line "Please input 10 numbers to be added together, pressing enter after each one.")
 
+; Get 10 integers inputted by the user
 (dotimes (itr 10)
     (setf (aref num-array itr) (read))
 )
 
+; Define function
 (defun add-array (arr)
+    ; Get length of array
     (setf len (length arr))
     (setf sum 0)
+    ; Iterate over entire array and add value to sum
     (dotimes (n len)
         (setf sum (+ sum (aref arr n)))
     )
@@ -19,4 +24,5 @@
 (write "Your array: ")
 (write num-array)
 (terpri)
+; Call funciton
 (add-array num-array)
