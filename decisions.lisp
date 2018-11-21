7946ad60b46be23844d5c9fbd01f3e988a2a9bff
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-11-20 23:53:54 -0500

    Add comments to decisions

--- decisions.lisp
+++ decisions.lisp
@@ -1,6 +1,8 @@
+; Get user age input
 (write-line "Please type your name, then press enter")
 (defvar age (read))
 
+; Check if age is less than 21
 (if (< age 21)
     (write-line "You're a minor! No drinking for you")
     (write-line "You're over 21! Enjoy your craft beers")
