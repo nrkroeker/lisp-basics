ef31e16843b035890fa2bba2492ae5b2204b8ae3
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-11-20 22:26:43 -0500

    Implement looping example using loop and when

--- loops.lisp
+++ loops.lisp
@@ -1,1 +1,10 @@
+(write-line "Please input the number you would like me to count up to, then press enter")
+(defvar count (read))
 
+(defvar itr 0)
+(loop
+     (setq itr (+ itr 1))
+     (write itr)
+     (terpri)
+     (when (= count itr) (return itr))
+ )
