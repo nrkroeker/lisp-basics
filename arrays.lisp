577df5cd68d697e656b349613d24e2d8eb938b77
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-11-20 23:53:21 -0500

    Add comments to arrays

--- arrays.lisp
+++ arrays.lisp
@@ -1,6 +1,9 @@
 (write-line "Input 10 numbers to put into an array, pressing enter after each one")
 
+; Define array of length 10
 (setf num-array (make-array'(10)))
+
+; Iterate from 0 to 9 and read an integer to place at that position in the array
 (setf itr 0)
 (loop
     (setf (aref num-array itr) (read))
@@ -8,6 +11,7 @@
     (setf itr (+ itr 1))
 )
 
+; Print resulting array
 (write "Your array: ")
 (write num-array)
 (terpri)
